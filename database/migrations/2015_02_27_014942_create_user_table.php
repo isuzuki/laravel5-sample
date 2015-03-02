a958ddd7c3351c1decdc40c50bd88ff1dae2a2fb
isuzuki <isuzuki@suzukiikuonoAir.lan>
2015-03-02 23:05:17 +0900

    Userテーブル作成のマイグレーション追加

---
+++ database/migrations/2015_02_27_014942_create_user_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUserTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('user', function($table) {
+			$table->string('id', 16);
+			$table->timestamps();
+			$table->timestamp('deleted_at');
+
+			$table->primary('id');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('user');
+	}
+
+}
